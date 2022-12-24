5110c2153f2586dfb430569abb34ec9ca228c4c2
jingwei950 <jingwei950@hotmail.sg>
2022-12-24 13:35:32 +0800

    Changed favicon and fix bug that banner doesn't show

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -14,7 +14,8 @@ function Banner() {
             const request = await axios.get(requests.fetchNetflixOriginals)
             setMovie(request.data.results[
                 Math.floor(Math.random() * request.data.results.length - 1)
-            ])
+            ]);
+
         }
 
         fetchData();
@@ -25,21 +26,14 @@ function Banner() {
         return string?.length > n ? string.substr(0, n - 1) + '...' : string;
     }
 
-    console.log(`https://image.tmdb.org/t/p/original${movie?.backdrop_path}`)
 
   return (
     <header 
         className='banner'
         style={{
-            // paddingTop:'400px',
             backgroundSize: "cover",
-            backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
+            backgroundImage: `url('https://image.tmdb.org/t/p/original${movie.backdrop_path ? movie.backdrop_path : movie.poster_path}')`,
             backgroundPosition: "center center",
-            // backgroundSize: "100% auto",
-            // backgroundRepeat: "no repeat",
-            // width: "100%",
-            // maxWidth: "1880px"
-            // height: "auto"
         }}
     >
         <div className="banner_contents">
